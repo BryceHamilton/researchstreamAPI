4748585e270d0d9d09a35b53ad9804a2de3ac9cb
BryceHamilton <bhamilton@haivision.com>
2020-12-31 13:35:10 +0000

    fixed passport typo

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -5,8 +5,8 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
   new GoogleStrategy(
     {
-      clientID: proccess.env.GOOGLE_CLIENT_ID,
-      clientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: '/auth/google/redirect',
     },
     (accessToken) => {
